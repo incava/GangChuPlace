bc6a6eb4bdb31310aacf7a58623094c0fff0afdb
incava <ingi1118@naver.com>
2023-03-29 17:27:50 +0900

    feat : gangchu click 기능 구현

--- app/src/main/java/com/incava/gangchuplace/view/main/GangChuFragment.kt
+++ app/src/main/java/com/incava/gangchuplace/view/main/GangChuFragment.kt
@@ -1,11 +1,26 @@
 package com.incava.gangchuplace.view.main
 
+import android.content.Intent
+import android.util.Log
+import androidx.navigation.fragment.findNavController
 import com.incava.gangchuplace.R
 import com.incava.gangchuplace.base.BaseFragment
 import com.incava.gangchuplace.databinding.FragmentGangChuBinding
+import com.incava.gangchuplace.view.search.SearchActivity
 
 class GangChuFragment : BaseFragment<FragmentGangChuBinding>(R.layout.fragment_gang_chu){
+
     override fun init() {
 
+        binding.toolbar.setOnMenuItemClickListener {
+            when (it.itemId) {
+                R.id.search -> startActivity(Intent(requireActivity(),SearchActivity::class.java)) // 검색창으로 이동
+                R.id.map -> findNavController().navigate(R.id.action_gangChuFragment_to_mapFragment) // map으로 이동.
+                else -> Log.i("Menuitem", "error")
+            }
+            true
+        }
     }
+
+
 }
---
+++ app/src/main/java/com/incava/gangchuplace/view/search/StoreSearchFragment.kt
@@ -0,0 +1,18 @@
+package com.incava.gangchuplace.view.search
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.incava.gangchuplace.R
+import com.incava.gangchuplace.base.BaseFragment
+import com.incava.gangchuplace.databinding.FragmentStoreSearchBinding
+
+
+class StoreSearchFragment : BaseFragment<FragmentStoreSearchBinding>(R.layout.fragment_store_search) {
+    override fun init() {
+
+    }
+
+}

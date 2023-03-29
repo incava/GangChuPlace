3dc4fd312dedd15fd0c61ec6f338f9cf2f1d42bc
incava <ingi1118@naver.com>
2023-03-29 14:56:43 +0900

    feat : Main bottomNavigation 구현

--- app/src/main/java/com/incava/gangchuplace/view/MainActivity.kt
+++ app/src/main/java/com/incava/gangchuplace/view/MainActivity.kt
@@ -2,11 +2,28 @@ package com.incava.gangchuplace.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.incava.gangchuplace.R
+import com.incava.gangchuplace.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        initBinding()
+        initNavigation()
+    }
+    private fun initNavigation() {
+        // NavController 획득
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host) as NavHostFragment
+        val navController = navHostFragment.findNavController()
+        binding.bottomNav.setupWithNavController(navController)
+    }
+    private fun initBinding() {
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.lifecycleOwner = this
     }
 }

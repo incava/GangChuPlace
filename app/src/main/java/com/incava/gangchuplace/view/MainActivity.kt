fea598467ba885dd2b81f18b5be862dde22853b1
incava <ingi1118@naver.com>
2023-03-29 13:20:41 +0900

    Initial commit

---
+++ app/src/main/java/com/incava/gangchuplace/view/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.incava.gangchuplace.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.incava.gangchuplace.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}

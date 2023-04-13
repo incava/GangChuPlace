c450eaaa47ca9e9cacefa43583a78c84142aa534
incava <ingi1118@naver.com>
2023-04-13 14:04:22 +0900

    feat : 툴바 네비게이션 연동

--- app/src/main/java/com/incava/gangchuplace/adapter/ReviewPageBindingAdapter.kt
+++ app/src/main/java/com/incava/gangchuplace/adapter/ReviewPageBindingAdapter.kt
@@ -1,13 +1,29 @@
 package com.incava.gangchuplace.adapter
 
+import android.widget.Toast
 import androidx.appcompat.widget.Toolbar
 import androidx.databinding.BindingAdapter
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.incava.gangchuplace.model.ReviewInfo
 
+
+/**
+ * 툴바에 이벤트가 필요해 바인딩으로 속성추가.
+ */
 object ReviewPageBindingAdapter {
 
-//    @JvmStatic
-//    @BindingAdapter("setReviewPageToolbar")
-//    fun setReviewPageToolbar(view : Toolbar, vm : ){
-//
-//    }
+
+    //Todo 툴바 링크 전송 관련.
+    @JvmStatic
+    @BindingAdapter("setReviewPageToolbar")
+    fun setReviewPageToolbar(view : Toolbar,reviewInfo: ReviewInfo){
+        view.apply {
+            setupWithNavController(findNavController())
+            setOnMenuItemClickListener {
+                Toast.makeText(context, "공유 클릭!", Toast.LENGTH_SHORT).show()
+                true
+            }
+        }
+    }
 }

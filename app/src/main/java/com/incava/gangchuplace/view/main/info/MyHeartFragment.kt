217e3a98d8a26d9e26f9b6edf5afa6e6eccbbefc
incava <ingi1118@naver.com>
2023-04-17 11:23:26 +0900

    feat : 찜하기 툴바 적용

--- app/src/main/java/com/incava/gangchuplace/view/main/info/MyHeartFragment.kt
+++ app/src/main/java/com/incava/gangchuplace/view/main/info/MyHeartFragment.kt
@@ -3,6 +3,8 @@ package com.incava.gangchuplace.view.main.info
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.incava.gangchuplace.R
 import com.incava.gangchuplace.base.BaseFragment
 import com.incava.gangchuplace.databinding.FragmentMyHeartBinding
@@ -11,7 +13,11 @@ import com.incava.gangchuplace.viewmodel.GangChuViewModel
 class MyHeartFragment : BaseFragment<FragmentMyHeartBinding>(R.layout.fragment_my_heart) {
     private val gangChuViewModel : GangChuViewModel by viewModels()
     override fun init() {
-        binding.gangChuVM = gangChuViewModel
+        binding.apply {
+            gangChuVM = gangChuViewModel
+            toolbar.setupWithNavController(findNavController())
+        }
+
     }
 
 }

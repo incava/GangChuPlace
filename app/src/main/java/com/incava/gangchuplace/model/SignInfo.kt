b8d4be02a7fdad8d18845590e531daf442c57e1d
incava <ingi1118@naver.com>
2023-04-18 10:25:19 +0900

    docs : 비밀번호를 User에 통합 정리

--- app/src/main/java/com/incava/gangchuplace/model/SignInfo.kt
+++
@@ -1,7 +0,0 @@
-package com.incava.gangchuplace.model
-
-data class SignInfo(
-    val id : String,
-    val password : String,
-    val nickname : String,
-)
--- app/src/main/java/com/incava/gangchuplace/model/User.kt
+++ app/src/main/java/com/incava/gangchuplace/model/User.kt
@@ -5,4 +5,5 @@ data class User (
     val image : String,
     val email : String,
     val loginRoute : String, // 로그인 경로 ex) 구글, 일반, 카카오
+    val password : String,
 )

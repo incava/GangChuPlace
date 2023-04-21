9faef1b63e9ca255c9d617f12279f9ad92f9d2d8
incava <ingi1118@naver.com>
2023-04-21 17:07:31 +0900

    feat : 중복으로 요청한 case 확인

--- app/src/main/java/com/incava/gangchuplace/viewmodel/FriendViewModel.kt
+++ app/src/main/java/com/incava/gangchuplace/viewmodel/FriendViewModel.kt
@@ -11,6 +11,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import androidx.navigation.findNavController
 import com.google.android.gms.tasks.Task
+import com.google.firebase.firestore.DocumentSnapshot
 import com.google.firebase.firestore.QuerySnapshot
 import com.incava.gangchuplace.adapter.Common.fireStore
 import com.incava.gangchuplace.adapter.Common.getSharedPreference
@@ -20,6 +21,8 @@ import com.incava.gangchuplace.model.User
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Deferred
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
 import kotlinx.coroutines.cancel
 import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.launch
@@ -75,28 +78,81 @@ class FriendViewModel : ViewModel() {
     }
 
     //요청한 사람의 이름이 있는지 확인
-    private suspend fun nameCheck(): Int {
+    private suspend fun nameCheck(): QuerySnapshot {
         return fireStore.collection("User")
             .whereEqualTo("nickname", friendName)
             .get()
             .await()
-            .size()
     }
 
     fun requestFriend(view: View) {
-        val user = getSharedPreference(view)
-        viewModelScope.launch {
+        try {
+            val user = getSharedPreference(view)
             CoroutineScope(Dispatchers.IO).launch {
-                if (friendName == user.nickname  || nameCheck() == 0) { //자신의 닉네임과 같거나 같은 이름이 없을 경우.
+                var friendId = nameCheck()
+                //자신의 닉네임과 같거나 같은 이름이 없을 경우.
+                if (friendName == user.nickname || friendId.documents.isEmpty()) {
                     CoroutineScope(Dispatchers.Main).launch {
                         showDialog(view, "친구 추가 실패", "등록된 닉네임이 없습니다.\n다시 한번 확인해 주세요.")
                     }
+                }else if(requestDuplicateCheck(friendId.documents[0].id,"${user.loginRoute}+${user.id}").documents.isNotEmpty()){
+                        //만약 이미 요청한 아이디가 있다면
+                    CoroutineScope(Dispatchers.Main).launch {
+                        showDialog(view, "이미 요청한 상태", "이미 친구 요청한 상태입니다.\n다시 한번 확인해 주세요.")
+                    }
                 }
-                else{
-                    dismissView(view)
+                else { // 친구할 닉네임을 찾았을 때
+                    val deffereds = listOf( // 두개의 요청이 완료 되기 위해 list로 묶어서 await시키기.
+                        async {
+                            uploadRequestFriend(
+                                friendId.documents[0].id,
+                                "${user.loginRoute}+${user.id}"
+                            )
+                        },
+                        async {
+                            uploadRequestedFriend(
+                                friendId.documents[0].id,
+                                "${user.loginRoute}+${user.id}"
+                            )
+                        }
+                    )
+                    deffereds.awaitAll()
+                    //모든 요청이 끝났을 때 이상이 없다면?
+                    CoroutineScope(Dispatchers.Main).launch {
+                        dismissView(view)
+                        Toast.makeText(view.context, "친구 추가 요청 완료!", Toast.LENGTH_SHORT).show()
+                    }
                 }
             }
+        } catch (e: Exception) {
+            Toast.makeText(view.context, "알 수 없는 원인으로 실패 하였습니다.", Toast.LENGTH_SHORT).show()
         }
+    }
+
+    private suspend fun uploadRequestFriend(friendId: String, myId: String) {
+        fireStore.collection("User")
+            .document(myId)
+            .collection("RequestFriend")
+            .add(mapOf("id" to friendId))
+            .await()
+    }
+
+    private suspend fun uploadRequestedFriend(friendId: String, myId: String) {
+        fireStore.collection("User")
+            .document(friendId)
+            .collection("RequestedFriend")
+            .add(mapOf("id" to myId))
+            .await()
+    }
+
+    //요청을 이미 했는지 확인.
+    private suspend fun requestDuplicateCheck(friendId :String, myId : String): QuerySnapshot {
+        return fireStore.collection("User")
+            .document(myId)
+            .collection("RequestFriend")
+            .whereEqualTo("id", friendId)
+            .get()
+            .await()
     }
 
 

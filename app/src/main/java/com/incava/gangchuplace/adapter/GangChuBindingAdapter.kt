89c106446f5385177309c9ee3544773900c4ee94
incava <68988975+incava@users.noreply.github.com>
2023-05-05 22:32:58 +0900

    feat : 거리 권한 거부 시 위치 안 보이게 설정.

--- app/src/main/java/com/incava/gangchuplace/adapter/GangChuBindingAdapter.kt
+++ app/src/main/java/com/incava/gangchuplace/adapter/GangChuBindingAdapter.kt
@@ -1,9 +1,13 @@
 package com.incava.gangchuplace.adapter
 
+import android.Manifest
 import android.R.attr.text
 import android.annotation.SuppressLint
+import android.content.pm.PackageManager
 import android.text.SpannableString
 import android.text.style.UnderlineSpan
+import android.util.Log
+import android.view.View
 import android.widget.TextView
 import androidx.databinding.BindingAdapter
 import com.incava.gangchuplace.adapter.Common.getSharedPreference
@@ -31,11 +35,15 @@ object GangChuBindingAdapter {
     @JvmStatic
     @BindingAdapter("translateDistance")
     fun translateDistance(view: TextView, distance: Int) {
-        view.text = "지금 거리에서 ${when (distance) {
-            in 0 until 1000 -> "$distance m"
-            else -> "${(((distance / 100.0).toInt())/10.0)} km"
-        }}"
+        // 위치 권한에 따라 view가 보이도록 or 보이지 않도록 하기.
+        if (view.context.checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_DENIED)
+            view.visibility = View.INVISIBLE else view.visibility = View.VISIBLE
+
+        view.text = "지금 거리에서 ${
+            when (distance) {
+                in 0 until 1000 -> "$distance m"
+                else -> "${(((distance / 100.0).toInt()) / 10.0)} km"
+            }
+        }"
     }
-
-
 }
